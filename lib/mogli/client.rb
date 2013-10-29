f4eb280c2c3541667ef276045f71d03fbc93634a
Jeremy Geros <jeremy453@gmail.com>
2013-10-28 19:59:01 -0600

    client fix

--- lib/mogli/client.rb
+++ lib/mogli/client.rb
@@ -180,7 +180,7 @@ module Mogli
       return extract_fetching_array(hash_or_array,klass) if is_fetching_array?(hash_or_array)
       # Facebook doesn't return numbers or booleans in an object or list
       # container; this will catch the number, "true" and "false" response case
-      return JSON.parse(hash_or_array) if hash_or_array.kind_of? String
+      # return JSON.parse(hash_or_array) if hash_or_array.kind_of? String
       return hash_or_array
     end
 
